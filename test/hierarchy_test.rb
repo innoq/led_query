fdc4382ff6ee899425faad203818e95b78a9e7e6
FND <fnd@localhost.localdomain>
2013-09-12 09:08:24 +0200

    restructured tests, disabling obsolete ones
    not using skip to avoid unnecessary setup & teardown

--- test/hierarchy_test.rb
+++ test/hierarchy_test.rb
@@ -3,6 +3,71 @@ require 'yaml'
 
 class HierarchyTest < DefaultTest
 
+  def test_concept_hierarchy
+    rdf = File.read(@common) + <<-EOS
+led:berlin a skos:Concept;
+    skos:inScheme led:locationScheme;
+    skos:broader led:germany;
+    skos:prefLabel "Berlin"@de.
+led:hamburg a skos:Concept;
+    skos:inScheme led:locationScheme;
+    skos:broader led:germany;
+    skos:prefLabel "Hamburg"@de.
+led:munich a skos:Concept;
+    skos:inScheme led:locationScheme;
+    skos:broader led:bavaria;
+    skos:prefLabel "München"@de.
+led:bavaria a skos:Concept;
+    skos:inScheme led:locationScheme;
+    skos:broader led:germany;
+    skos:prefLabel "Bayern"@de.
+led:germany a skos:Concept;
+    skos:inScheme led:locationScheme;
+    skos:prefLabel "Bundesrepublik Deutschland"@de.
+
+led:ammonium rdf:type skos:Concept;
+    skos:inScheme led:analyteScheme;
+    skos:prefLabel "Ammonium"@de.
+led:phosphorus a skos:Concept;
+    skos:inScheme led:analyteScheme;
+    skos:prefLabel "Phosphor"@de.
+
+led:obs123 a qb:Observation;
+    led:analyte led:ammonium;
+    led:location led:berlin.
+led:obs321 a qb:Observation;
+    led:analyte led:ammonium;
+    led:location led:berlin.
+led:obs456 a qb:Observation;
+    led:analyte led:phosphorus;
+    led:location led:hamburg.
+led:obs789 a qb:Observation;
+    led:analyte led:phosphorus;
+    led:location led:munich.
+    EOS
+    @store.add_triples @repo, "text/turtle", rdf
+    skos = File.expand_path("../fixtures/skos.ttl", __FILE__)
+    @store.add_triples @repo, "text/turtle", File.read(skos)
+
+    concepts, obs_count, hierarchy = @db.determine_concepts(["#{@led}location"],
+        {}, true, true)
+    assert_equal obs_count, 4
+    assert_equal concepts, {
+      "#{@led}location" => {
+        "#{@led}berlin" => { "de" => "Berlin" },
+        "#{@led}hamburg" => { "de" => "Hamburg" },
+        "#{@led}munich" => { "de" => "München" }
+      }
+    }
+    assert_equal hierarchy, YAML.load(<<-EOS)
+#{@led}germany:
+  #{@led}berlin: {}
+  #{@led}hamburg: {}
+  #{@led}bavaria:
+    #{@led}munich: {}
+    EOS
+  end
+
   def test_resolve_hierarchy
     skos = File.expand_path("../fixtures/skos.ttl", __FILE__)
     @store.add_triples @repo, "text/turtle", File.read(skos)
@@ -89,72 +154,7 @@ de:
     assert_equal expected, LEDQuery::Database.resolve_hierarchy(data)
   end
 
-  def test_concept_hierarchy
-    rdf = File.read(@common) + <<-EOS
-led:berlin a skos:Concept;
-    skos:inScheme led:locationScheme;
-    skos:broader led:germany;
-    skos:prefLabel "Berlin"@de.
-led:hamburg a skos:Concept;
-    skos:inScheme led:locationScheme;
-    skos:broader led:germany;
-    skos:prefLabel "Hamburg"@de.
-led:munich a skos:Concept;
-    skos:inScheme led:locationScheme;
-    skos:broader led:bavaria;
-    skos:prefLabel "München"@de.
-led:bavaria a skos:Concept;
-    skos:inScheme led:locationScheme;
-    skos:broader led:germany;
-    skos:prefLabel "Bayern"@de.
-led:germany a skos:Concept;
-    skos:inScheme led:locationScheme;
-    skos:prefLabel "Bundesrepublik Deutschland"@de.
-
-led:ammonium rdf:type skos:Concept;
-    skos:inScheme led:analyteScheme;
-    skos:prefLabel "Ammonium"@de.
-led:phosphorus a skos:Concept;
-    skos:inScheme led:analyteScheme;
-    skos:prefLabel "Phosphor"@de.
-
-led:obs123 a qb:Observation;
-    led:analyte led:ammonium;
-    led:location led:berlin.
-led:obs321 a qb:Observation;
-    led:analyte led:ammonium;
-    led:location led:berlin.
-led:obs456 a qb:Observation;
-    led:analyte led:phosphorus;
-    led:location led:hamburg.
-led:obs789 a qb:Observation;
-    led:analyte led:phosphorus;
-    led:location led:munich.
-    EOS
-    @store.add_triples @repo, "text/turtle", rdf
-    skos = File.expand_path("../fixtures/skos.ttl", __FILE__)
-    @store.add_triples @repo, "text/turtle", File.read(skos)
-
-    concepts, obs_count, hierarchy = @db.determine_concepts(["#{@led}location"],
-        {}, true, true)
-    assert_equal obs_count, 4
-    assert_equal concepts, {
-      "#{@led}location" => {
-        "#{@led}berlin" => { "de" => "Berlin" },
-        "#{@led}hamburg" => { "de" => "Hamburg" },
-        "#{@led}munich" => { "de" => "München" }
-      }
-    }
-    assert_equal hierarchy, YAML.load(<<-EOS)
-#{@led}germany:
-  #{@led}berlin: {}
-  #{@led}hamburg: {}
-  #{@led}bavaria:
-    #{@led}munich: {}
-    EOS
-  end
-
-  def test_inferences
+  def disabled_test_inferences # disabled as this merely tests the database
     rdf = <<-EOS.strip
 @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
 @prefix led: <http://data.uba.de/led/> .
@@ -203,7 +203,7 @@ SELECT DISTINCT ?child WHERE {
     end
   end
 
-  def test_transitivity
+  def disabled_test_transitivity # disabled as this merely tests the database
     rdf = <<-EOS.strip
 @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
 @prefix led: <http://data.uba.de/led/> .
